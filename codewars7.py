11200eebb1b7ab190a729884e614334184fed5b8
jatinyadav2036 <jatinya21@gmail.com>
2026-05-07 22:45:10 +0530

    7

---
+++ codewars7.py
@@ -0,0 +1,9 @@
+# Pete, the baker
+def cakes(recipe, available):
+    lst =[]
+    for key , value in recipe.items():
+        if key not in available:
+            return 0
+        lst.append(available[key] // value)
+    return min(lst)
+    
