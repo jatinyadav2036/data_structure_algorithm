5c388a8b5a52ac7f5d5760d1d7d3162599ffea38
jatinyadav2036 <jatinya21@gmail.com>
2026-05-08 23:33:50 +0530

    54

---
+++ leetcode54.py
@@ -0,0 +1,41 @@
+#54 Spiral Matrix
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if not matrix or not matrix[0]:
+            return []
+
+        result = []
+
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while top <= bottom and left <= right:
+
+            # Traverse left to right
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+
+            # Traverse top to bottom
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            # Traverse right to left
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+
+            # Traverse bottom to top
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+
+        return result 
