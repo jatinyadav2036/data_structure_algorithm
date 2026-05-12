4c1db9e8de1173064f634adfc96d1c66bd605e0b
jatinyadav2036 <jatinya21@gmail.com>
2026-05-12 22:10:24 +0530

    977

---
+++ leetcode977.py
@@ -0,0 +1,12 @@
+# 977 Squares of a Sorted Array
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        sq = [x**2 for x in nums]
+        sq.sort()
+        return sq
+        # return sorted([x**2 for x in nums])
+        
