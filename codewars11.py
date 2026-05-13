d3a65576b643448cb229f594532708b2b51b7da7
jatinyadav2036 <jatinya21@gmail.com>
2026-05-13 22:25:12 +0530

    11

---
+++ codewars11.py
@@ -0,0 +1,7 @@
+# Nesting Structure Comparison
+
+def same_structure_as(original,other):
+    if type(original) == list == type(other):
+        return len(original) == len(other) and all(map(same_structure_as , original , other))
+    else:
+        return type(original) != list != type(other)
