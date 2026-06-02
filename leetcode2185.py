d708f9e97806890afd7568c2270e5c3a5b980594
jatinyadav2036 <jatinya21@gmail.com>
2026-06-02 20:47:56 +0530

    2185

---
+++ leetcode2185.py
@@ -0,0 +1,14 @@
+# 2185. Counting Words With a Given Prefix
+
+class Solution(object):
+    def prefixCount(self, words, pref):
+        """
+        :type words: List[str]
+        :type pref: str
+        :rtype: int
+        """
+        count = 0
+        for i in words:
+            if i.startswith(pref):
+                count += 1
+        return count
