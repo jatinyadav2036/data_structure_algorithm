21cfa8ae51a1e2a4187aec85b024118c71dfd95e
jatinyadav2036 <jatinya21@gmail.com>
2026-06-23 22:15:10 +0530

    206

---
+++ leetcode206.py
@@ -0,0 +1,19 @@
+# 206. Reverse Linked List
+
+class Solution(object):
+    def reverseList(self, head):
+
+        prev = None
+        curr = head
+
+        while curr:
+
+            nxt = curr.next
+
+            curr.next = prev
+
+            prev = curr
+
+            curr = nxt
+
+        return prev
