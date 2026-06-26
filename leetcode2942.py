298b1faa6c2efb51642f951af5ac188368c89dec
jatinyadav2036 <jatinya21@gmail.com>
2026-06-26 17:51:23 +0530

    2942

---
+++ leetcode2942.py
@@ -0,0 +1,16 @@
+# 2942. Find Words Containing Character
+
+
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(len(words)):
+            if x in words[i] :
+                ans.append(i)
+        return ans
+        
