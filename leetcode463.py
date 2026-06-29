4a569d1a928ef123255e3b399c97f6e08fbdcd24
jatinyadav2036 <jatinya21@gmail.com>
2026-06-29 21:49:22 +0530

    463

--- leetcode463.py
+++ leetcode463.py
@@ -1,3 +1,23 @@
+# 463. Island Perimeter
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        n = len(grid)
+        m = len(grid[0])
+        perimeter = 0
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    perimeter += 4
+
+                    if i + 1 < n and grid[i + 1][j] == 1:
+                        perimeter -= 2
+
+                    if j + 1 < m and grid[i][j + 1] == 1:
+                        perimeter -= 2
+
+        return perimeter
     
 # class Solution(object):
 #     def islandPerimeter(self, grid):
