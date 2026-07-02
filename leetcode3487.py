8663c48e52b5f5411e6e9014b552cc7d85d75984
jatinyadav2036 <jatinya21@gmail.com>
2026-07-02 21:12:19 +0530

    3487

--- leetcode3487.py
+++ leetcode3487.py
@@ -1,4 +1,22 @@
+# 3487. Maximum Unique Subarray Sum After Deletion
+
 class Solution(object):
+    def maxSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if max(nums) < 0 :
+            return max(nums)
+        sm = 0
+        lst = list(set(nums))
+        for i in lst:
+            if i < 0 :
+                continue
+            sm += i
+        return sm
+        
+# class Solution(object):
 #     def maxSum(self, nums):
 #         posNumSum = set([num for num in nums if num > 0])
 #         return max(nums) if len(posNumSum) == 0 else sum(posNumSum)
