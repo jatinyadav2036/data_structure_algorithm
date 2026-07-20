b16e3a2fabac1dc6aecf3fc547a8e7b8cd6d2995
jatinyadav2036 <jatinya21@gmail.com>
2026-07-20 22:23:15 +0530

    771

---
+++ leetcode771.py
@@ -0,0 +1,15 @@
+# 771. Jewels and Stones
+
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        cnt = 0
+        for i in stones:
+            if i in jewels:
+                cnt += 1
+        return cnt
+        
