c46af7e5c8d6b11d755d8a6da33af183f6c93159
jatinyadav2036 <jatinya21@gmail.com>
2026-07-26 23:11:53 +0530

    665

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,1 @@
-class Solution(object):
-#     def distMoney(self, money, children):
-#         if money < children:
-#             return -1
-
-#         money -= children
-#         ans = min(money // 7, children)
-
-#         money -= ans * 7
-#         children -= ans
-
-#         if children == 0 and money > 0:
-#             ans -= 1
-#         elif children == 1 and money == 3:
-#             ans -= 1
-
-#         return ans
+object
