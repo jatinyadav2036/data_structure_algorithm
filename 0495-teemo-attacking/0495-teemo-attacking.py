00b59055f92a0eed0bc7fd0f943814720d1f5a70
Jatin Yadav <jatinya21@gmail.com>
2026-07-29 22:13:05 +0530

    Time: 17 ms (72.53%), Space: 13.1 MB (76.6%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findPoisonedDuration(self, timeSeries, duration):
+        sm = 0
+        for i in range(len(timeSeries)-1):
+            if timeSeries[i] + duration <= timeSeries[i+1]:
+                sm += duration
+            else:
+                sm += timeSeries[i+1] - timeSeries[i]
+        sm += duration
+        
+        return sm
