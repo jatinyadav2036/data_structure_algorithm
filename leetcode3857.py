dd1533d90e197fa430132dd7fbce606da1c41b1f
jatinyadav2036 <jatinya21@gmail.com>
2026-07-30 21:13:38 +0530

    3857

---
+++ leetcode3857.py
@@ -0,0 +1,13 @@
+# 3857. Minimum Cost to Split into Ones
+
+class Solution(object):
+    def minCost(self, n):
+        return ((n)*(n-1))//2
+
+# class Solution(object):
+#     def minCost(self, n):
+#         ans = 0
+#         while n > 0:
+#             ans += n - 1
+#             n -= 1
+#         return ans
